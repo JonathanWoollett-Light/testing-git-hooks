a359a43e204ec14aaef266e46d203547268a9969
Jonathan Woollett-Light <jonathanwoollettlight@gmail.com>
2022-06-06 10:23:00 +0100

    Update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,12 @@
+# For every staged file
 for i in $(git diff --name-only --cached); do
+    # Get the extension
     filename=$(basename -- "$i")
     extension="${filename##*.}"
     if [ "$extension" = "rs" ]; then
+        # Run `cargo fmt` for this file
         rustfmt $i --config format_code_in_doc_comments=true,format_strings=true,license_template_path=./license.txt,imports_granularity=Module,normalize_comments=true,normalize_doc_attributes=true,group_imports=StdExternalCrate
-
+        # Run `cargo-clippy` for this file
         cargo-clippy $i -- -D warnings \
             -D clippy::as_conversions \
             -D clippy::map_err_ignore \
@@ -14,7 +17,9 @@ for i in $(git diff --name-only --cached); do
             -D clippy::wildcard_imports
     fi
     if [ "$extension" == "py" ]; then
+        # Run `black` for this file
         black $i
     fi
+    # Add changes to this file (as a result of formatting) to the commit.
     git add $i
 done
