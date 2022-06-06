b918b89bf084db65b7217ff79eb3af78adab0598
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2022-06-06 16:14:05 +0100

    clippy test
    clippy test 2

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ fn main() {
     // let _j: i64 = i as i64;
     let _s = "assssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdfdsfsssssssasdasdsdsdesdssssssss";
     let x = 2;
-    let _t = 2;
+    let t = 2;
     let _a = 2;
     let y = 4;
     println!("{}{}", x, y);
+    let u = 2;
     println!("bruh");
     let _x = 2;
 }
