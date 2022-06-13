099292e1ea85aa3985810ba6c0530da6cf29f841
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2022-06-13 10:20:32 +0100

    Updated

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Exit immediately when encountering a non-zero command
+set -e 
 # I don't think there is a reasonable way to do these more specifically
 cargo audit
 # For every staged file
@@ -10,18 +11,20 @@ for i in $(git diff --name-only --cached); do
     if [ "$extension" = "rs" ]; then
         # Maybe also consider https://crates.io/crates/cargo-spellcheck
         # TODO We need to have some discussion on what configs to use for `fmt` and `clippy`.
-        # TODO Put the fmt config arguments in different lines.
+        
+        # Read rustfmt config, replace '\n' with ','
+        rustfmt_config_1="$(sed -z "s/\n/,/g;s/,$/\n/" fmt.toml)"
+        # Remove '"'
+        rustfmt_config_2="${rustfmt_config_1//\"}"
         # Run `cargo fmt` for this file
-        rustfmt $i --config comment_width=100,wrap_comments=true,format_code_in_doc_comments=true,format_strings=true,license_template_path=./license.txt,imports_granularity=Module,normalize_comments=true,normalize_doc_attributes=true,group_imports=StdExternalCrate
+        rustfmt $i --config $rustfmt_config_2
+
+        # Read clippy config, prefix ` -D clippy::` to each line
+        clippy_config_1="$(sed -e 's/^/ -D clippy::/' clip.toml)"
+        # Remove all instances of `=true\n`
+        clippy_config_2="${clippy_config_1//$'=true\n'}"
         # Run `cargo-clippy` for this file
-        cargo-clippy $i -- -D warnings \
-            -D clippy::as_conversions \
-            -D clippy::map_err_ignore \
-            -D clippy::large_types_passed_by_value \
-            -D clippy::missing_docs_in_private_items \
-            -D clippy::used_underscore_binding \
-            -D clippy::wildcard_dependencies \
-            -D clippy::wildcard_imports
+        cargo-clippy $i -- -D warnings $clippy_config_2
     fi
     if [ "$extension" == "py" ]; then
         # Run `black` for this file
@@ -30,3 +33,5 @@ for i in $(git diff --name-only --cached); do
     # Add changes to this file (as a result of formatting) to the commit.
     git add $i
 done
+
+exit 1
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@
 //! cdsf
 // something
 /// docing this
+/// ```
+/// let x = 2;
+/// let a = 3;
+/// ```
 fn main() {
     println!("Hello, world!");
     let _i = 12u32;
     let a = 2;
     let b = 23;
+    let c: u32 = b as u32;
     // let _j: i64 = i as i64;
     let _s = "assssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdfdsfsssssssasdasdsdsdesdssssssss";
     let x = 2;
